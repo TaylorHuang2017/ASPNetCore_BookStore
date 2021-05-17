95ea2b6bc33f3c216d7f0dde2f20626bfc1f2c29
Taylor Huang <tahuan@microsoft.com>
2021-05-17 19:48:08 +0800

    add getbook (view details)

--- Tahuan.BookStore/Tahuan.BookStore/Models/BookModel.cs
+++ Tahuan.BookStore/Tahuan.BookStore/Models/BookModel.cs
@@ -12,6 +12,9 @@ namespace Tahuan.BookStore.Models
         public string Author { get; set; }
 
         public string Description { get; set; }
+        public string Category { get; set; }
+        public string Language { get; set; }
+        public int TotalPages { get; set; }
 
     }
 }
--- Tahuan.BookStore/Tahuan.BookStore/Repository/BookRepository.cs
+++ Tahuan.BookStore/Tahuan.BookStore/Repository/BookRepository.cs
@@ -28,12 +28,12 @@ namespace Tahuan.BookStore.Repository
         {
             return new List<BookModel>()
             {
-                new BookModel(){ Id =1, Title = "MVC", Author = "Nitish", Description = "Model View Controller"},
-                new BookModel(){ Id =2, Title = "DotNet Core", Author = "Nitish", Description = ".NET core 3.1"},
-                new BookModel(){ Id =3, Title = "C#", Author = "Monika", Description = "C sharp language"},
-                new BookModel(){ Id =4, Title = "Java", Author = "Taylor", Description = "Java web programming"},
-                new BookModel(){ Id =5, Title = "Php", Author = "Frank", Description = "Php is here"},
-                new BookModel(){ Id =5, Title = "Azure DevOps", Author = "Frank", Description = "DevOps is the future"},
+                new BookModel(){ Id =1, Title = "MVC", Author = "Nitish", Description = "Model View Controller", Category="Programming", Language="English", TotalPages=568},
+                new BookModel(){ Id =2, Title = "DotNet Core", Author = "Nitish", Description = ".NET core 3.1", Category="Programming", Language="German", TotalPages=1200},
+                new BookModel(){ Id =3, Title = "C#", Author = "Monika", Description = "C sharp language", Category="Programming", Language="English", TotalPages=908},
+                new BookModel(){ Id =4, Title = "Java", Author = "Taylor", Description = "Java web programming", Category="Programming", Language="Chinese", TotalPages=210},
+                new BookModel(){ Id =5, Title = "Php", Author = "Frank", Description = "Php is here", Category="Programming", Language="English", TotalPages=340},
+                new BookModel(){ Id =5, Title = "Azure DevOps", Author = "Frank", Description = "DevOps is the future", Category="DevOps", Language="Spanish", TotalPages=689},
             };
         }
     }
